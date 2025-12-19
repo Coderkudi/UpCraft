8571014c95cff59226cf3f54e579419e2617cd95
Coderkudi <goyalrimmi12@gmail.com>
2025-12-19 14:53:32 +0530

    fix: trash code fix in app.jsx and navbar.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,81 +1,61 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-// import PrivateRoute from "./components/PrivateRoute";
-// import Login from "./pages/Login";
-// import Profile from "./pages/Profile";
-// import Register from "./pages/Register";
-// import ChangePassword from "./pages/changePassword";
-// import UpdateProfile from "./pages/updateProfile";
-// import Navbar from "./components/Navbar";
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+import ChangePassword from "./pages/changePassword";
+import UpdateProfile from "./pages/updateProfile";
+import Navbar from "./components/Navbar";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import CourseList from "./pages/CourseList";
+import CourseDetail from "./pages/CourseDetail";
+import Lesson from "./pages/Lesson";
+import Quiz from "./pages/Certificate";
+import Certificate from "./pages/Certificate";
 
-//         <Route
-//           path="/profile"
-//           element={
-//             <PrivateRoute>
-//               <Profile />
-//             </PrivateRoute>
-//           }
-//         />
-
-//         <Route
-//           path="/change-password"
-//           element={
-//             <PrivateRoute>
-//               <ChangePassword />
-//             </PrivateRoute>
-//           }
-//         />
-
-//         <Route
-//           path="/update-profile"
-//           element={
-//             <PrivateRoute>
-//               <UpdateProfile />
-//             </PrivateRoute>
-//           }
-//         />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-// import Profile from './pages/Profile';
-// import CourseList from './pages/CourseList';
-import Courses from './pages/Courses';
-import CourseDetail from './pages/CourseDetail'; // Renamed from DemoCourse
-import Lesson from './pages/Lesson';
-// import Quiz from './pages/Quiz';
-// import Certificate from './pages/Certificate';
-
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-gray-950 text-white">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/courses" element={<Courses />} />
-          {/* <Route path="/profile" element={<Profile />} /> */}
-          <Route path="/course/:courseId" element={<CourseDetail />} />
-          <Route path="/course/:courseId/lesson/:lessonId" element={<Lesson />} />
-          {/* <Route path="/course/:courseId/quiz" element={<Quiz />} />
-          <Route path="/course/:courseId/certificate" element={<Certificate />} /> */}
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/change-password"
+          element={
+            <PrivateRoute>
+              <ChangePassword />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/update-profile"
+          element={
+            <PrivateRoute>
+              <UpdateProfile />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/course/:courseId" element={<CourseDetail />} />
+        <Route path="/course/:courseId/lesson/:lessonId" element={<Lesson />} />
+      </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;
+// <Route path="/" element={<Home />} />
